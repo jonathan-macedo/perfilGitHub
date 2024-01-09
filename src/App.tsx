a053020703cd3f7aa178825dac29815103563de3
Jonathan Macedo <jonathan_maccas@outlook.com>
2024-01-09 08:45:37 -0300

    feat(update): alterando o titulo inicial da página

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import classes from './App.module.css'
 function App() {
   return (
     <div className={classes.app}>
-      <h1>Hello GitHub</h1>
+      <h1>Encontrar perfil GitHub</h1>
       <Outlet />
     </div>
   )
