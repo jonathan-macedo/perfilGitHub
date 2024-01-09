b5a32252011db2451ec455632fe3d37286353b1b
Jonathan Macedo <jonathan_maccas@outlook.com>
2024-01-09 08:21:02 -0300

    chore(create): criando arquivo de erro, caso o usuário não seja válido

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react"
+import { Error } from "../components/Error"
 import { Search } from "../components/Search"
 import User from "../components/User"
 import { UserProps } from "../types/user"
 
 export const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
+    
     const res = await fetch(`https://api.github.com/users/${userName}`)
     const data = await res.json()
+
+    if (res.status === 404) {
+      setError(true)
+      return
+    }
 
     const { avatar_url, login, location, followers, following } = data
 
@@ -26,6 +36,7 @@ export const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   )
 }
